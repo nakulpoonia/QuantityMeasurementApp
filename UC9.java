482719d1f56a2c4362a7e49f1ff9b361de628a17
Nakul Poonia <123162638+nakulpoonia@users.noreply.github.com>
2026-03-19 21:17:22 +0530

    Update

--- UC9.java
+++ UC9.java
@@ -109,17 +109,16 @@ public class UC9 {
         QuantityWeight a = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
         QuantityWeight b = new QuantityWeight(1000.0, WeightUnit.GRAM);
 
-        System.out.println(a.equals(b)); // true
+        System.out.println(a.equals(b)); 
 
-        System.out.println(a.convertTo(WeightUnit.POUND)); // ~2.20462 lb
+        System.out.println(a.convertTo(WeightUnit.POUND));
 
-        System.out.println(a.add(b)); // 2 kg
+        System.out.println(a.add(b)); 
 
-        System.out.println(a.add(b, WeightUnit.GRAM)); // 2000 g
-
+        System.out.println(a.add(b, WeightUnit.GRAM)); 
         QuantityWeight c = new QuantityWeight(2.0, WeightUnit.POUND);
         QuantityWeight d = new QuantityWeight(1.0, WeightUnit.KILOGRAM);
 
-        System.out.println(c.add(d, WeightUnit.POUND)); // ~4.409 lb
+        System.out.println(c.add(d, WeightUnit.POUND)); 
     }
 }
