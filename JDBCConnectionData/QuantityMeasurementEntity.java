b3a253d429573b1be27d52d8294485a23f642b61
Nakulpoonia <poonianakul@gmail.com>
2026-04-02 22:51:10 +0530

    UC16

---
+++ JDBCConnectionData/QuantityMeasurementEntity.java
@@ -0,0 +1,25 @@
+package com.example.JDBCConnectionData;
+
+
+
+    public class QuantityMeasurementEntity {
+        private String operation;
+        private double value;
+
+        public QuantityMeasurementEntity(String operation, double value) {
+            this.operation = operation;
+            this.value = value;
+        }
+
+        public String getOperation() {
+            return operation;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        public String toString() {
+            return operation + " = " + value;
+        }
+    }
